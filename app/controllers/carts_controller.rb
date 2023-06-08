5cc351fe1eb4e8544d29b2b21d9f904534960410
Palash_Yadav <ypalash67@gmail.com>
2023-06-08 18:50:19 +0530

    Kaminari gem

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,10 @@
 class CartsController < ApplicationController
   def index
+    @cart=current_user.carts.first.cart_items
+  end
+  def addproduct
+    @cartitems= current_user.carts.first.cart_items.new(product_id:params[:id])
+    @cartitems.save
+    render :index
   end
 end
