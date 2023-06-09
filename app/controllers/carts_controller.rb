71a5b724c0fedbaf532f9b2ccc8ddbe20817b311
Palash_Yadav <ypalash67@gmail.com>
2023-06-09 18:53:33 +0530

    fourth

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,6 +5,10 @@ class CartsController < ApplicationController
   def addproduct
     @cartitems= current_user.carts.first.cart_items.new(product_id:params[:id])
     @cartitems.save
-    render action: :index
+    redirect_to carts_path, notice: 'Item added to cart successfully.'
+  end
+  def removeproduct
+    @cartitemscurrent= current_user.carts.first.cart_items[:id]
+    @cartitemscurrent.destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#index"
   get '/addproduct/:id', to: 'carts#addproduct',as:'addproduct' 
+  get '/removeproduct/:id', to: 'carts#removeproduct',as:'removeproduct'
 end
